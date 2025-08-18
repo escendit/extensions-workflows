a86f977996aec7cd593f8ac15c9a82ce84178e9d
Simon Novak <25969+snovak7@users.noreply.github.com>
2025-08-18 08:43:39 +0200

    feat: add Temporal workflows extensions with basic builders, defaults, and service collection integrations

---
+++ src/Workflows/ITemporalBuilder.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the Escendit GmbH under one or more agreements.
+// The Escendit GmbH licenses this file to you under the Apache License 2.0.
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+using Options;
+using Temporalio.Client;
+using Temporalio.Extensions.Hosting;
+
+/// <summary>
+/// Represents a builder interface for constructing temporal workflow configurations or objects.
+/// </summary>
+public interface ITemporalBuilder
+{
+    /// <summary>
+    /// Gets the name.
+    /// </summary>
+    /// <value>The name.</value>
+    public string Name { get; }
+
+    /// <summary>
+    /// Gets the services.
+    /// </summary>
+    /// <value>The services.</value>
+    public IServiceCollection Services { get; }
+
+    /// <summary>
+    /// Gets the client options builder.
+    /// </summary>
+    /// <value>The client options.</value>
+    internal OptionsBuilder<TemporalClientConnectOptions> ClientOptionsBuilder { get; }
+
+    /// <summary>
+    /// Gets the worker builder.
+    /// </summary>
+    /// <value>The workflow builder.</value>
+    internal ITemporalWorkerServiceOptionsBuilder WorkerOptionsBuilder { get; }
+}
---
+++ src/Workflows/TemporalBuilder.cs
@@ -0,0 +1,45 @@
+﻿// Licensed to the Escendit GmbH under one or more agreements.
+// The Escendit GmbH licenses this file to you under the Apache License 2.0.
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+using Options;
+using Temporalio.Client;
+using Temporalio.Extensions.Hosting;
+
+/// <summary>
+/// Provides a builder class for configuring and managing Temporal services, clients, and workers.
+/// </summary>
+internal sealed class TemporalBuilder : ITemporalBuilder
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TemporalBuilder"/> class.
+    /// </summary>
+    /// <param name="name">The name.</param>
+    /// <param name="serviceCollection">The service collection.</param>
+    /// <param name="clientOptionsBuilder">The client options builder.</param>
+    /// <param name="workerOptionsBuilder">The worker options builder.</param>
+    public TemporalBuilder(
+        string name,
+        IServiceCollection serviceCollection,
+        OptionsBuilder<TemporalClientConnectOptions> clientOptionsBuilder,
+        ITemporalWorkerServiceOptionsBuilder workerOptionsBuilder)
+    {
+        Name = name;
+        Services = serviceCollection;
+        ClientOptionsBuilder = clientOptionsBuilder;
+        WorkerOptionsBuilder = workerOptionsBuilder;
+    }
+
+    /// <inheritdoc/>
+    public string Name { get; }
+
+    /// <inheritdoc/>
+    public IServiceCollection Services { get; }
+
+    /// <inheritdoc/>
+    public OptionsBuilder<TemporalClientConnectOptions> ClientOptionsBuilder { get; }
+
+    /// <inheritdoc/>
+    public ITemporalWorkerServiceOptionsBuilder WorkerOptionsBuilder { get; }
+}
